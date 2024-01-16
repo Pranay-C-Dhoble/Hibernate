98b8cb8bf6382adcee781ddb1637bcebbfaa9b65
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2024-01-16 20:40:48 +0530

    Implementation Of @Embeddable Annotation
    Implementation Of @Embeddable Annotation

---
+++ src/main/java/demohibernate/demoApp.java
@@ -0,0 +1,30 @@
+package demohibernate;
+
+import demohibernate.entity.Certificate;
+import demohibernate.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class demoApp {
+    public static void main(String[] args) {
+        System.out.println("Program Started .....");
+        Configuration cfg = new Configuration();
+        cfg.configure("demohibernate.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Swaraj");
+        student.setCity("Nagpur");
+        student.setCertificate(new Certificate("Java", "2 months"));
+
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(student);
+        tx.commit();
+        session.close();
+        System.out.println("Program Ended .....");
+    }
+}
---
+++ src/main/java/demohibernate/entity/Certificate.java
@@ -0,0 +1,35 @@
+package demohibernate.entity;
+
+import javax.persistence.Embeddable;
+
+@Embeddable //@Embeddable is used to mark the class as an embeddable class
+//Which means that this class will be embedded in another class
+//In this case, this class will be embedded in Student class
+public class Certificate {
+    private String course;
+    private String duration;
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public Certificate(String course, String duration) {
+        this.course = course;
+        this.duration = duration;
+    }
+    public Certificate(){
+        super();
+    }
+}
